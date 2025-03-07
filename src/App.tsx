e116376deed76666c42662552567ff575cce19af
ujjwal105 <ujjwaltyagi2969@gmail.com>
2025-03-07 12:14:54 +0530

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button className="p-6 text-2xl cursor-pointer">Billify</Button>
+    </div>
+  );
+}
+
+export default App;

bc6030f22499da4e54f25c9fdb0e6c24b5aa2881
ujjwal105 <ujjwaltyagi2969@gmail.com>
2025-03-11 17:59:59 +0530

    add signature modal

--- src/Pages/InvoiceCard/ShippingPartner.tsx
+++ src/Pages/InvoiceCard/ShippingPartner.tsx
@@ -4,8 +4,26 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { PaymentInfoSchema, PaymentInfoType } from "@/lib/schema";
 import { Form } from "@/components/ui/form";
+import SignatureCanvas from "react-signature-canvas";
+import { FilePen } from "lucide-react";
+import { Eraser } from "lucide-react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import clsx from "clsx";
+import { Input } from "@/components/ui/input";
 
 function ShippingPartner({ activeState }: any) {
+  const sigCanvas = useRef<SignatureCanvas>(null);
+  const [color, setColor] = useState("black");
   const handleSubmit = () => {
     console.log("Form submitted");
   };
@@ -17,6 +35,19 @@ function ShippingPartner({ activeState }: any) {
       accountNumber: "",
     },
   });
+  const clearSignature = () => {
+    sigCanvas?.current?.clear();
+  };
+
+  const handleColorChange = (e: any) => {
+    setColor(e.target.value);
+  };
+  const colors = ["black", "red", "green", "blue"];
+  const handleSnapShot = () => {
+    const data = sigCanvas?.current?.toDataURL();
+    console.log(data);
+  };
+
   return (
     <Accordion
       title="Payment Information & Additional"
@@ -56,6 +87,59 @@ function ShippingPartner({ activeState }: any) {
               />
             </div>
             <h1 className="text-base font-bold mt-4">Summary</h1>
+            <div>
+              <Dialog>
+                <DialogTrigger className="flex flex-col border-2 rounded-md p-6 items-center text-sm font-medium gap-2 cursor-pointer hover:border-blue-400 mt-4">
+                  <FilePen />
+                  Click to add signature
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Signature</DialogTitle>
+                    <DialogDescription className="bg-gray-200 rounded-lg mt-3">
+                      <SignatureCanvas
+                        ref={sigCanvas}
+                        penColor={color}
+                        minWidth={2}
+                        canvasProps={{
+                          width: 460,
+                          height: 300,
+                          className: "sigCanvas",
+                        }}
+                      />
+                    </DialogDescription>
+                    <div className="flex justify-between items-center mt-4">
+                      <Button onClick={clearSignature} variant={"outline"}>
+                        <Eraser />
+                        Erase
+                      </Button>
+                      <div onClick={handleColorChange} className="flex gap-2">
+                        {colors.map((color) => {
+                          return (
+                            <div>
+                              <Input
+                                type="radio"
+                                value={color}
+                                name="color"
+                                onChange={handleColorChange}
+                                className={clsx(
+                                  "border-0 rounded-full h-5 w-5",
+                                  color === "red" && "text-red-500",
+                                  color === "blue" && "accent-blue-700",
+                                  color === "green" && "accent-green-600",
+                                  color === "black" && "accent-black"
+                                )}
+                              />
+                            </div>
+                          );
+                        })}
+                      </div>
+                      <Button onClick={handleSnapShot}>Done</Button>
+                    </div>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </div>
           </div>
         </form>
       </Form>
@@ -64,4 +148,3 @@ function ShippingPartner({ activeState }: any) {
 }
 
 export default ShippingPartner;
-
